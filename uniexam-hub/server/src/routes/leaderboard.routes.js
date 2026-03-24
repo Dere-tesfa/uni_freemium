bd0aa27067ab02d76bb41996d2b85098ad781b85
Dere <deretesfa6@gmail.com>
2026-03-23 20:34:31 -0800

    update folder structure

--- uniexam-hub/server/src/routes/leaderboard.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getLeaderboard } = require('../controllers/leaderboard.controller');
-const { protect } = require('../middleware/auth.middleware');
-
-// @route   GET /api/leaderboard
-router.get('/', protect, getLeaderboard);
-
-module.exports = router;
